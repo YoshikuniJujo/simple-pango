c714c6edc872da06207e01cd7d5ef4428b54534e
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2021-06-14 16:56:01 +0900

    add source codes

---
+++ src/System/Glib/Quarks.hs
@@ -0,0 +1,7 @@
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
+
+module System.Glib.Quarks (
+	GQuark,
+	gQuarkFromString, gQuarkToString, gInternString, gUninternString ) where
+
+import System.Glib.Quarks.Internal
