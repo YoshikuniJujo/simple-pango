4e1649ad5212e539712a5bc3170df32e5cd5aa8e
Yoshikuni Jujo <yoshikuni.jujo.pc@gmail.com>
2021-06-15 09:48:21 +0900

    use glib-stopgap

--- src/System/GLib/Quarks.hs
+++
@@ -1,7 +0,0 @@
-{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
-
-module System.GLib.Quarks (
-	GQuark,
-	gQuarkFromString, gQuarkToString, gInternString, gUninternString ) where
-
-import System.GLib.Quarks.Internal
